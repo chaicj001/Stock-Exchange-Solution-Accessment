6c2c907d31a1300393e96b2d034486cc5fee719f
chaicj001 <cjchai001@mymail.sim.edu.sg>
2023-11-01 23:16:01 +0800

    socket problem been solved
    sender/client now can send multiple time of message that server can catch but only in one string, " " (space will let the server recognize as send multiple time)
    more fix and algorithm will be release in future

--- connect2.cpp
+++ connect2.cpp
@@ -3,44 +3,65 @@
 #include <string>
 
 using namespace std;
+SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-int main() {
+
+void listenserver(SOCKET clientSocket) {
+    char buffer[1024];
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead > 0) {
+        buffer[bytesRead] = '\0';
+        cout << "Received from server: " << buffer << endl;
+    }
+}
+SOCKET connectToServer() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         cerr << "WSAStartup failed" << endl;
-        return 1;
+        return INVALID_SOCKET;
     }
 
-    while (true) {
-        SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-        if (clientSocket == INVALID_SOCKET) {
-            cerr << "Socket creation failed" << endl;
-            WSACleanup();
-            return 1;
-        }
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Socket creation failed" << endl;
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
 
-        sockaddr_in serverAddress;
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
-        serverAddress.sin_port = htons(12345); // Server port
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
+    serverAddress.sin_port = htons(12345); // Server port
 
-        if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
-            cerr << "Connection failed" << endl;
-            closesocket(clientSocket);
-            WSACleanup();
-            return 1;
-        }
-
-        string message = "hi server spam here";
-        send(clientSocket, message.c_str(), message.size(), 0);
-
-        // Close the connection for this message
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        cerr << "Connection failed" << endl;
         closesocket(clientSocket);
-
-        // Optional: Sleep or add a delay before sending the next message
-        // Sleep(1000); // Sleep for 1 second
+        WSACleanup();
+        return INVALID_SOCKET;
     }
 
-    WSACleanup();
+    return clientSocket;
+}
+
+int main() {
+    string username;
+    cin >> username;
+    while (true){
+        string message= username+":";
+        string extramessage;
+        SOCKET clientSocket = connectToServer();
+        if (clientSocket == INVALID_SOCKET) {
+            return 1;
+        }
+        
+        cin >> extramessage;
+        message += extramessage ;
+        send(clientSocket, message.c_str(), message.size(), 0);
+        listenserver(clientSocket);
+
+
+        closesocket(clientSocket);
+        WSACleanup();
+    }
     return 0;
 }
